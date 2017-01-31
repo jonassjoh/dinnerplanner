30a79abcb53ae7ac87465688b5334f819494390e
jonassjoh <jonassjoh@gmail.com>
2017-01-31 16:14:31 +0100

    workpass 1 yo

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
@@ -1,10 +1,10 @@
 package se.kth.csc.iprog.dinnerplanner.android;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
-
-import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
-
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -12,13 +12,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         // Default call to load previous state
         super.onCreate(savedInstanceState);
-
-        // Set the view for the main activity screen
-        // it must come before any call to findViewById method
         setContentView(R.layout.activity_main);
 
-        // Creating the view class instance
-        ExampleView mainView = new ExampleView(findViewById(R.id.this_is_example_view_id));
+        getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getActionBar().setCustomView(R.layout.titlebar);
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.chose_menu);
+            }
+        });
     }
 
 }

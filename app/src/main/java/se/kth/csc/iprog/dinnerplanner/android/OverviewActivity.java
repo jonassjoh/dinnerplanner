89e048021189809533a9ff6fde723e587bf3681b
Kenneth Runnman <kenneth.runnman@gmail.com>
2017-02-07 12:57:41 +0100

    monday progress retry

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/OverviewActivity.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/OverviewActivity.java
@@ -1,8 +1,46 @@
 package se.kth.csc.iprog.dinnerplanner.android;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.test.ActivityTestCase;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by kenneth on 2/6/17.
  */
 
-public class OverviewActivity {
+public class OverviewActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        // Default call to load previous state
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.overview_view);
+
+        getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getActionBar().setCustomView(R.layout.titlebar);
+
+        View view = findViewById(R.id.overview_item);
+        ImageView iView = (ImageView) view.findViewById(R.id.item_image);
+        iView.setImageResource(R.drawable.sourdough);
+
+        TextView tView = (TextView) view.findViewById(R.id.item_title);
+        tView.setText(R.string.ingredients);
+
+        RecyclerView overview = (RecyclerView) findViewById(R.id.overview_item);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        overview.setLayoutManager(linearLayoutManager);
+
+        //MenuAdapter overviewAdapter = new MenuAdapter(this, getStarters());
+
+        //overview.setAdapter(startersAdapter);
+
+
+    }
 }

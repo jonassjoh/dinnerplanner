e46d2b728c2b6f8a84da835b84ba01610b64f99f
jonassjoh <jonassjoh@gmail.com>
2017-02-18 12:28:15 +0100

    initial lab4
    basic

---
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/AsyncData.java
@@ -0,0 +1,5 @@
+package se.kth.csc.iprog.dinnerplanner.android;
+
+public interface AsyncData {
+    public void onData(Object data);
+}
---
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/SpoonacularAPIClient.java
@@ -0,0 +1,19 @@
+package se.kth.csc.iprog.dinnerplanner.android;
+
+import com.loopj.android.http.*;
+
+public class SpoonacularAPIClient {
+    private static final String BASE_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/";
+    private static final String API_KEY = "Qu9grxVNWpmshA4Kl9pTwyiJxVGUp1lKzrZjsnghQMkFkfA4LB";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.addHeader("mashape-key",API_KEY);
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
